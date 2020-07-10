b1377e6fdfb82d7b3e8b246200295391bcb3950e
arseniyx92 <arseniykhlytchiev@gmail.com>
2020-07-10 16:57:09 +0300

    cool

--- main.go
+++ main.go
@@ -6,22 +6,31 @@ import (
 	"fmt"
 	"html/template"
 	"io"
-	"io/ioutil"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
-	"path/filepath"
 	"strings"
 
-	uuid "github.com/satori/go.uuid"
-
+	"cloud.google.com/go/storage"
 	_ "github.com/go-sql-driver/mysql"
+	uuid "github.com/satori/go.uuid"
 	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/net/context"
+	"google.golang.org/api/iterator"
+	"google.golang.org/appengine"
 )
 
 var tpl *template.Template
 var db *sql.DB
 var dbSession = map[string]string{}
+
+var (
+	storageClient *storage.Client
+
+	// Set this in app.yaml when running in production.
+	bucket = os.Getenv("photo-blog-282118.appspot.com")
+)
 
 //Feed is ...
 type Feed struct {
@@ -43,6 +52,12 @@ func init() {
 }
 
 func main() {
+	ctx := context.Background()
+	var err error
+	storageClient, err = storage.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 	http.HandleFunc("/", index)
 	http.HandleFunc("/signup", signup)
 	http.HandleFunc("/login", login)
@@ -51,7 +66,7 @@ func main() {
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.Handle("/stylesheets/", http.StripPrefix("/stylesheets", http.FileServer(http.Dir("./stylesheets"))))
 	http.Handle("/public/pics/", http.StripPrefix("/public/pics", http.FileServer(http.Dir("./public/images"))))
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	check(err)
 }
 
@@ -64,6 +79,8 @@ func post(w http.ResponseWriter, req *http.Request) {
 		http.Redirect(w, req, "/", http.StatusSeeOther)
 	}
 	// main
+	feed := Feed{}
+	feed.User = dbSession[c.Value]
 	c = &http.Cookie{
 		Name:  "feed",
 		Value: "",
@@ -72,26 +89,59 @@ func post(w http.ResponseWriter, req *http.Request) {
 		mf, fh, err := req.FormFile("nf")
 		check(err)
 		defer mf.Close()
-		// creating a sha
-		ext := strings.Split(fh.Filename, ".")[1]
-		h := sha1.New()
-		io.Copy(h, mf)
-		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
 		// creating new file
-		wd, err := os.Getwd()
+		//connecting to gcloud
+		fname, err := uploadFile(req, mf, fh)
 		check(err)
-		path := filepath.Join(wd, "public", "images", fname)
-		nf, err := os.Create(path)
-		check(err)
-		defer nf.Close()
+		// wd, err := os.Getwd()
+		// check(err)
+		// path := filepath.Join(wd, "public", "images", fname)
+		// nf, err := os.Create(path)
+		// check(err)
+		// defer nf.Close()
 		// copy
-		mf.Seek(0, 0)
-		io.Copy(nf, mf)
-		c = appendValue(w, c, fname)
+		// mf.Seek(0, 0)
+		// io.Copy(nf, mf)
+		ctx := appengine.NewContext(req)
+		obj, err := storageClient.Bucket(bucket).Object(fname).Attrs(ctx)
+		check(err)
+		c = appendValue(w, c, obj.MediaLink)
 	}
 	xs := strings.Split(c.Value, "|")
 	xs = append(xs[1:])
-	tpl.ExecuteTemplate(w, "post.gohtml", xs)
+	feed.Pics = xs
+	tpl.ExecuteTemplate(w, "post.gohtml", feed)
+}
+
+func uploadFile(req *http.Request, mf multipart.File, fh *multipart.FileHeader) (string, error) {
+	//getting extension
+	ext, err := fileFilter(req, fh)
+	check(err)
+	//creating a sha
+	h := sha1.New()
+	io.Copy(h, mf)
+	fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+	//putting file
+	mf.Seek(0, 0)
+	ctx := appengine.NewContext(req)
+	return fname, putFile(ctx, fname, mf)
+}
+
+func putFile(ctx context.Context, fname string, rdr io.Reader) error {
+	sw := storageClient.Bucket(bucket).Object(fname).NewWriter(ctx)
+	io.Copy(sw, rdr)
+	return sw.Close()
+}
+
+func fileFilter(req *http.Request, fh *multipart.FileHeader) (string, error) {
+	//ext := strings.Split(fh.Filename, ".")[1]
+	ext := fh.Filename[strings.LastIndex(fh.Filename, ".")+1:]
+
+	switch ext {
+	case "jpg", "jpeg", "txt", "md":
+		return ext, nil
+	}
+	return ext, fmt.Errorf("We do not allow files of type %s. We only allow jpg, jpeg, txt, md extensions", ext)
 }
 
 func appendValue(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
@@ -115,12 +165,6 @@ func logout(w http.ResponseWriter, req *http.Request) {
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	// c, err := req.Cookie("session")
-	// if err == http.ErrNoCookie {
-	// 	tpl.ExecuteTemplate(w, "index.gohtml", nil)
-	// 	return
-	// }
-	// tpl.ExecuteTemplate(w, "index.gohtml", dbSession[c.Value])
 	feed := Feed{}
 	c, err := req.Cookie("session")
 	if err == http.ErrNoCookie {
@@ -132,13 +176,18 @@ func index(w http.ResponseWriter, req *http.Request) {
 		Name:  "feed",
 		Value: "",
 	}
-	wd, err := os.Getwd()
-	check(err)
-	path := filepath.Join(wd, "public", "images")
-	files, err := ioutil.ReadDir(path)
-	check(err)
-	for _, file := range files {
-		c = appendValue(w, c, file.Name())
+	ctx := appengine.NewContext(req)
+	query := &storage.Query{Prefix: ""}
+	it := storageClient.Bucket(bucket).Objects(ctx, query)
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		c = appendValue(w, c, attrs.MediaLink)
 	}
 	xs := strings.Split(c.Value, "|")
 	xs = append(xs[1:])
@@ -167,7 +216,7 @@ func signup(w http.ResponseWriter, req *http.Request) {
 	}
 
 	db, err := sql.Open("mysql", "arseniyx92:123@unix(/cloudsql/photo-blog-282118:europe-west6:photo-blog-users)/users?charset=utf8")
-	// db, err := sql.Open("mysql", "arseniyx92:123@tcp(34.65.166.197)/users?charset=utf8")
+	//db, err := sql.Open("mysql", "arseniyx92:123@tcp(34.65.166.197)/users?charset=utf8")
 	check(err)
 	defer db.Close()
 	err = db.Ping()
@@ -228,7 +277,7 @@ func login(w http.ResponseWriter, req *http.Request) {
 	}
 
 	db, err := sql.Open("mysql", "arseniyx92:123@unix(/cloudsql/photo-blog-282118:europe-west6:photo-blog-users)/users?charset=utf8")
-	// db, err := sql.Open("mysql", "arseniyx92:123@tcp(34.65.166.197)/users?charset=utf8")
+	//db, err := sql.Open("mysql", "arseniyx92:123@tcp(34.65.166.197)/users?charset=utf8")
 	check(err)
 	defer db.Close()
 	err = db.Ping()
